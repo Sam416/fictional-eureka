75c8d8323c0db00ec3278a69eaf225a2695a3f69
Sam416 <sam.korth.1234@gmail.com>
2019-10-29 15:15:00 -0500

    First title check with enter added

--- Main.py
+++ Main.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-user_name = input("Email: ")
-user_pass = input("Password: ")
+
+user_name = "samuel.j.korth-1@ou.edu"
+user_pass = "testpass1"
 driver = webdriver.Chrome("C:\\Users\\Sam\\Downloads\\chromedriver_win32\\chromedriver.exe")
-driver.get("https://hudl.com/login")
-element = driver.find_element_by_id("email")
-element.send_keys(user_name)
-element = driver.find_element_by_id("password")
-element.send_keys(Keys.RETURN)
+def title_check_enter():
+    driver.get("https://hudl.com/login")
+    email = driver.find_element_by_id("email")
+    password = driver.find_element_by_id("password")
+    email.send_keys(user_name)
+    password.send_keys(user_pass)
+    password.send_keys(Keys.RETURN)
+    WebDriverWait(driver, 10).until(EC.title_contains("Home"))
+    assert driver.title == "Home - Hudl", "Homepage title correct after login w/enter key"
+driver.close()
