90c7fa32a1f1199f1b9d689260097946f5af9415
Sam416 <sam.korth.1234@gmail.com>
2019-10-31 12:42:52 -0500

    Changed to reading sensitive user info from file

--- Main.py
+++ Main.py
@@ -5,9 +5,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
-user_name = "samuel.j.korth-1@ou.edu"
-user_pass = "testpass1"
+blobs = open("blobs.txt")
+user_name = blobs.readLine()
+user_pass = blobs.readLine()
+user_badpass = blobs.readLine()
+blobs.close()
+
 driver = webdriver.Chrome("C:\\Users\\Sam\\Downloads\\chromedriver_win32\\chromedriver.exe")
+
+
 def title_check_enter():
     driver.get("https://hudl.com/login")
     email = driver.find_element_by_id("email")
@@ -17,4 +23,20 @@ def title_check_enter():
     password.send_keys(Keys.RETURN)
     WebDriverWait(driver, 10).until(EC.title_contains("Home"))
     assert driver.title == "Home - Hudl", "Homepage title correct after login w/enter key"
+
+
+def title_check_click():
+    driver.get("https://hudl.com/login")
+    email = driver.find_element_by_id("email")
+    password = driver.find_element_by_id("password")
+    login = driver.find_element_by_id("logIn")
+    email.send_keys(user_name)
+    password.send_keys(user_pass)
+    login.click()
+    WebDriverWait(driver, 10).until(EC.title_contains("Home"))
+    assert driver.title == "Home - Hudl", "Homepage title correct after login w/ button click"
+
+
+title_check_click()
 driver.close()
+
--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Hudl Code Test
 
 To run this code, you must have the selenium package installed in your Python environment.
 Also, ensure that the executable chromedriver is on the system path.
+
+Running this code requires that you have a file on your project path
+named "blobs.txt" in the following format:
+    
+    username
+    password
+    incorrect password
+ 
